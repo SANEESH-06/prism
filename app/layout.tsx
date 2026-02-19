ed5c183bdd0a49edac1a19248d08e0e90fba36da
saneesh <saneeshn6@gmail.com>
2026-02-20 02:01:57 +0530

    Add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import PageLoader from "./components/page-loader";
@@ -16,7 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 /**
- * SEO + Google Verification for prism-333.vercel.app
+ * SEO + Google Verification
  */
 export const metadata: Metadata = {
   metadataBase: new URL("https://prism-333.vercel.app"),
@@ -96,11 +97,39 @@ export default function RootLayout({
       <head>
         <link rel="canonical" href="https://prism-333.vercel.app" />
         <meta name="theme-color" content="#ffffff" />
+
+        {/* Google Tag Manager Script */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NWJ98HBW');
+            `,
+          }}
+        />
       </head>
 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NWJ98HBW"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <ThemeProvider attribute="class" defaultTheme="light">
           <PageLoader />
           <Toaster position="bottom-right" richColors />
