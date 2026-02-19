3604cd01d0058876420a101a0f3a745f1c0d2684
saneesh <saneeshn6@gmail.com>
2026-02-19 16:40:40 +0530

    Fix middleware for Google verification

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,23 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// This function can be marked `async` if using `await` inside
 export function middleware(_request: NextRequest) {
   return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: [
     /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+     Match all paths EXCEPT these:
+     - api routes
+     - Next.js static files
+     - image optimization files
+     - favicon
+     - robots.txt
+     - sitemap.xml
+     - google verification files
+     - all public static files
+    */
+    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|google.*|.*\\.(png|jpg|jpeg|svg|gif|webp)$).*)",
   ],
 };
