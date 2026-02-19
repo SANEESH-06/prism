91cdb3c93339438393c9eb79c13c70ce29e4e91b
saneesh <saneeshn6@gmail.com>
2026-02-20 01:39:40 +0530

    fix robots

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://prismlaundry.com/sitemap.xml",
+    sitemap: "https://prism-333.vercel.app/sitemap.xml",
   };
 }
